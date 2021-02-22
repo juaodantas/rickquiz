cecc0fb361037ecc2281ec4c2153192f2fea9ec8
Joao vitor dantas <jvdantasilva@hotmail.com>
2021-02-22 13:34:14 -0400

    Aula 3

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,16 @@
 import React from 'react';
-import styled from 'styled-components'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 
 import db from '../db.json'
 import Widget from '../src/components/Widget'
-// import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import QuizBackground from '../src/components/QuizBackground'
 import Input from '../src/components/Input'
 import Button from '../src/components/Button'
+import QuizContainer from '../src/components/QuizContainer'
 
 
-// import QuizLogo from '../src/components/QuizLogo'
-
-
-
-// const BackgroundImage = styled.div`
-// background-image: url(${db.bg});
-// flex: 1;
-// background-size: cover;
-// background-position: center;
-// `;
-
-export const QuizContainer = styled.div`
-width: 100%;
-max-width: 350px;
-padding-top: 45px;
-margin: auto 10%;
-@media screen and (max-width: 500px) {
-  margin: auto;
-  padding:15px;
-}
-`;
 export default function Home() {
   const router = useRouter();
   const [name, setName] = React.useState('');
